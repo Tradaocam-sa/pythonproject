26a98736265b4132e16f7ebe70f486ac963a88fd
Do Duy Anh <doduyanh9803@gmail.com>
2023-03-31 02:05:46 +0700

    update gui

--- Managerfuntions.py
+++ Managerfuntions.py
@@ -1,28 +1,118 @@
 import tkinter as tk
-# from tkinter import ttk
+from tkinter import ttk
 import ttkbootstrap as ttk
 
 
-class Management:
-    def __init__(self, root):
-        self.window = root
-        self.window.title("Transcritp information")
-        self.window.geometry("1140x700")
+class Management(tk.Tk):
+    def __init__(self, title, size, minsize):
+            super().__init__()
+        #SETUP
+            self.title(title)
+            self.geometry(f'{size[0]}x{size[1]}')
+            self.minsize(minsize[0],minsize[1])
+
+        #widget
+            self.left = leftBackground(self)
+            self.right = rightBackground(self)
+        #RUN
+
+            self.mainloop()
+
+            
+class leftBackground(ttk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        ttk.Label(self,background='#A4C2F1').pack(expand = True, fill='both')
+        self.place(x = 0, y = 0, relwidth= 0.3, relheight=1)
+
+        #Button one
+        frame1 = ttk.Frame(self)
+        show_std_info_buton = ttk.Button(frame1, text = 'Show students', width=30)
+
+        show_std_info_buton.pack(expand= True, fill='both',  padx=0, pady = 0)
         
-        self.detail_frame = tk.Frame(
-            self.window,
-            bg='#CBDCF7',
-            relief=ttk.GROOVE
-        )
-        self.detail_frame.place(width=260, height=700)
+        frame1.place(relx=0.15, rely= 0.05, )
+        
+        
+        #Button two
+        frame2 = ttk.Frame(self)
+        show_info_course = ttk.Button(frame2, text = 'Show courses', width = 30 )
+        
+        show_info_course.pack(expand= True, fill='both',  padx=0, pady = 0)
+        frame2.place(relx=0.15, rely= 0.1 )
+        
+        #Button 3
+        frame3 = ttk.Frame(self)
+        Add_student = ttk.Button(frame3, text = 'Add student', width = 30 )
+        
+        Add_student.pack(expand= True, fill='both',  padx=0, pady = 0)
+        frame3.place(relx=0.15, rely= 0.15 )
+        
+        #Button 4
+        frame4 = ttk.Frame(self)
+        Add_mark = ttk.Button(frame4, text = 'Add mark', width = 30 )
+        
+        Add_mark.pack(expand= True, fill='both',  padx=0, pady = 0)
+        frame4.place(relx=0.15, rely= 0.2 )
+        
+        #Button 5
+        frame5 = ttk.Frame(self)
+        Add_course = ttk.Button(frame5, text = 'Add course', width = 30 )
+        
+        Add_course.pack(expand= True, fill='both',  padx=0, pady = 0)
+        frame5.place(relx=0.15, rely= 0.25 )
+        
+        
+        #Button 6
+        frame6 = ttk.Frame(self)
+        Edit_info_course = ttk.Button(frame6, text = 'Edit information of course', width = 30 )
+        
+        Edit_info_course.pack(expand= True, fill='both',  padx=0, pady = 0)
+        frame6.place(relx=0.15, rely= 0.3 )
+        
+        #Button 7
+        frame7 = ttk.Frame(self)
+        edit_student_info = ttk.Button(frame7, text = 'Edit infomation of student', width = 30 )
+        
+        edit_student_info.pack(expand= True, fill='both',  padx=0, pady = 0)
+        frame7.place(relx=0.15, rely= 0.35 )
+        
+        #Button 8
+        frame8 = ttk.Frame(self)
+        Edit_mark = ttk.Button(frame8, text = 'Edit mark', width = 30 )
+        
+        Edit_mark.pack(expand= True, fill='both',  padx=0, pady = 0)
+        frame8.place(relx=0.15, rely= 0.4 )
         
         
         
 
+class rightBackground(ttk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        ttk.Label(self,background='#CBDCF7').pack(expand = True, fill='both')
+        self.place(relx= 0.3, y = 0, relwidth= 0.7, relheight=1)
+        
+        
+        #Title
+        frame1 = ttk.Frame(self)
+        title = ttk.Label(frame1, text='TRANSCRIPT INFORMATION MANAGEMENT SYSTEM', font = ('Inria' , 20, 'bold' ), background='#CBDCF7', foreground='#568DE5' )
+        
+        title.pack(expand=True, fill= 'both', padx = 0, pady= 0) 
+        frame1.place(relx=0.15, y=0, relwidth= 1, relheight= 0.05)
+        
+    def display_course_list(self):
+        # hide the input buttons if they are visible
+        self.add_std_btn.place_forget()
+        self.add_crs_btn.place_forget()
+        self.add_mark_btn.place_forget()
 
-if __name__ == "__main__":
-    root = tk.Tk()
-    obj = Management(root)
-    root.mainloop()
+        # display the course list label
+        self.display_label.config(text="This is the course list")
+        self.display_label.place(x=500, y=300)
+
+        
+
+Management('Transcript', '1540x700+0+0',(1540,700))
         
     
